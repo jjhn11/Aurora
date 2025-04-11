91b12923ffd7e8f2553a6bcc9233ea9df300f20e
Feli-deli <a22490375@itmexicali.edu.mx>
2025-04-11 13:55:03 -0700

    HeroVideo
    Se hizo el HeroVideo de la pagina de eventos

--- frontend/src/views/EventsView.vue
+++ frontend/src/views/EventsView.vue
@@ -1,7 +1,23 @@
 <script setup>
-
+import HeroVideo from '@/components/Events/HeroVideo.vue';
 </script>
 
 <template>
-
+<HeroVideo />
+<br><br>
+<div class="container">
+    <h1 class="title">CULTURALES</h1>
+    <!--Carrusel-->
+    <br><br>
+</div>
+<div class="container">
+    <h1 class="title">ESCOLARES</h1>
+    <!--Carrusel-->
+    <br><br>
+</div>
+<div class="container">
+    <h1 class="title">DEPORTIVOS</h1>
+    <!--Carrusel-->
+    <br><br>
+</div>
 </template>
