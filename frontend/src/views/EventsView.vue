a711f3a179c57eccb4ae475946119d60c628b656
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-04-14 23:05:11 -0700

    Revert "Events carousel (#2)"
    This reverts commit 2857c81b8650215ed70ece36192bed1e56f03f47.

--- frontend/src/views/EventsView.vue
+++ frontend/src/views/EventsView.vue
@@ -1,29 +1,7 @@
 <script setup>
-import HeroVideo from '@/components/Events/HeroVideo.vue';
-import Carousel1 from '@/components/Events/Carousel1.vue';
-import Carousel2 from '@/components/Events/Carousel2.vue';
-import Carousel3 from '@/components/Events/Carousel3.vue';
+
 </script>
 
 <template>
-<HeroVideo />
-<br><br>
-<div class="container">
-    <h1 class="title">CULTURALES</h1>
-    <!--Carrusel-->
-    <Carousel1 />
-    <br><br>
-</div>
-<div class="container">
-    <h1 class="title">ESCOLARES</h1>
-    <!--Carrusel-->
-    <Carousel2 />
-    <br><br>
-</div>
-<div class="container">
-    <h1 class="title">DEPORTIVOS</h1>
-    <!--Carrusel-->
-    <Carousel3 />
-    <br><br>
-</div>
+
 </template>
