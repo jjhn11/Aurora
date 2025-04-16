8610117bac4c46837d0302fa8f78a9d23e847ae7
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-04-16 10:24:54 -0700

    V1 backend navbar (#14)
    * store
    
    * Login sin docker
    
    * fix: login and logout working along docker-compose
    
    * fix 2.0: login and logout working along docker-compose
    
    * fix 3.0: login, logout and status working along docker-compose
    
    * feature: login and logout with front
    missing: check status
    
    * Front-end ready for logging in and out.
    features: back-end communication with front-end for user information after logging in with google.
    errors: profile display design is broken... maybe check out viewStack and currentView in Navbar.vue?
    
    * navbar component ok
    
    ---------
    
    Co-authored-by: Adrián Arvizu <a22490466@itmexicali.edu.mx>
    Co-authored-by: zNeoj <jhosttin6@hotmail.com>
    Co-authored-by: zNeoj <88008021+zNeoj@users.noreply.github.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+// // Configure axios
+const API_BASE_URL = 'http://localhost:3000'; // Adjust this to your API base URL
+axios.defaults.baseURL = API_BASE_URL; // Set the base URL for axios requests
+axios.defaults.withCredentials = true; // Include credentials in requests by default
 
 // Import Bootstrap CSS and JS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 // Icons
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(axios).use(store).use(router).mount('#app')
