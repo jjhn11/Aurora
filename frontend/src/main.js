2beee7eb8ee7071b52c559002848ecc04f4d9715
zNeoj <jhosttin6@hotmail.com>
2025-05-06 17:35:35 -0700

    You can now log-in with Google using the hosted app.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,19 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-// // Configure axios
-const API_BASE_URL = 'http://localhost:3000'; // Adjust this to your API base URL
+// Vite proporciona estas variables automáticamente
+const isProduction = import.meta.env.PROD;
+const mode = import.meta.env.MODE;
+
+// Seleccionar la URL base según el entorno
+const API_BASE_URL = isProduction 
+  ? 'http://168.231.73.137:3000'  // URL de producción
+  : 'http://localhost:3000';      // URL de desarrollo
+
+  alert(`App running in ${isProduction ? 'production' : 'development'} mode`);
+  alert(`${mode} mode`);
+
+// Configure axios
 axios.defaults.baseURL = API_BASE_URL; // Set the base URL for axios requests
 axios.defaults.withCredentials = true; // Include credentials in requests by default
 
