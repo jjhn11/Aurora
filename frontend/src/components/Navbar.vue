75b9f57e3091a1944bffbdaadc751c6700d3021e
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-05-16 19:24:20 -0700

    Update Navbar.vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,7 +10,7 @@
       
       const adminEmails = [
           "a22490408@itmexicali.edu.mx",
-          "a22490390@itmexicali.edu.mx",
+          "a22490396@itmexicali.edu.mx",
           "a22490388@itmexicali.edu.mx",
           "a22490378@itmexicali.edu.mx",
           "a22490352@itmexicali.edu.mx",
