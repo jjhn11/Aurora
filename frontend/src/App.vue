b8c123cd278f8e1b2099d84ad03fe36cdbcf92e5
Misa815 <marnleasim17@gmail.com>
2025-05-18 17:05:50 +0000

    Cambios que se hicieron manualmente a la maquina

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -77,3 +77,20 @@ onUnmounted(() => {
   }
 }
 </style>
+
+<script type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67c2cfe5b20e19cdb7e91eed' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
